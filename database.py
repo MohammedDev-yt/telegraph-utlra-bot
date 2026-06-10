17cb5bb6a8a2ca4e79c21358875eb65013789464
MD-Developer-yt <animeworldday2005@gmail.com>
2026-06-10 09:05:40 +0530

    database.py

--- database.py
+++ database.py
@@ -18,14 +18,12 @@ db = client[config.DB_NAME]
 
 users = db.users
 posts = db.posts
-batches = db.batches
 user_images = db.user_images
 
 # ---------------- INDEXES ---------------- #
 
 users.create_index("user_id", unique=True)
 posts.create_index("user_id")
-batches.create_index("user_id")
 user_images.create_index("user_id")
 
 # ---------------- USER ---------------- #
@@ -53,15 +51,6 @@ def save_post(user_id, url, title):
         "time": int(time.time())
     })
 
-# ---------------- BATCH ---------------- #
-
-def save_batch(user_id, urls):
-    batches.insert_one({
-        "user_id": user_id,
-        "urls": urls,
-        "time": int(time.time())
-    })
-
 # ---------------- STATS ---------------- #
 
 def total_users():
@@ -69,84 +58,6 @@ def total_users():
 
 def total_posts():
     return posts.count_documents({})
-
-def total_batches():
-    return batches.count_documents({})
-
-# ------------------------- #
-# Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
-# ------------------------- #
-
-from pymongo import MongoClient
-import config
-import time
-
-# ---------------- CONNECTION ---------------- #
-
-client = MongoClient(config.MONGO_URL, serverSelectionTimeoutMS=5000)
-
-db = client[config.DB_NAME]
-
-# ---------------- COLLECTIONS ---------------- #
-
-users = db.users
-posts = db.posts
-batches = db.batches
-user_images = db.user_images
-
-# ---------------- INDEXES ---------------- #
-
-users.create_index("user_id", unique=True)
-posts.create_index("user_id")
-batches.create_index("user_id")
-user_images.create_index("user_id")
-
-# ---------------- USER ---------------- #
-
-def add_user(user_id):
-    if not user_id:
-        return
-
-    users.update_one(
-        {"user_id": user_id},
-        {"$set": {
-            "user_id": user_id,
-            "time": int(time.time())
-        }},
-        upsert=True
-    )
-
-# ---------------- POSTS ---------------- #
-
-def save_post(user_id, url, title):
-    posts.insert_one({
-        "user_id": user_id,
-        "url": url,
-        "title": title,
-        "time": int(time.time())
-    })
-
-# ---------------- BATCH ---------------- #
-
-def save_batch(user_id, urls):
-    batches.insert_one({
-        "user_id": user_id,
-        "urls": urls,
-        "time": int(time.time())
-    })
-
-# ---------------- STATS ---------------- #
-
-def total_users():
-    return users.count_documents({})
-
-def total_posts():
-    return posts.count_documents({})
-
-def total_batches():
-    return batches.count_documents({})
 
 # ------------------------- #
 # Don't Remove Credit 
